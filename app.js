9bfa36d5632d96c48ea81a5287b390bde6792325
echai13 <cher13@brandeis.edu>
2017-09-19 12:01:18 -0400

    stylesheets and routing

--- app.js
+++ app.js
@@ -1,33 +1,20 @@
 const express = require('express');
 const app = express();
 const nunjucks = require('nunjucks');
+const routes = require('./routes');
+app.use('/', routes);
 
-var locals = {
-    title: 'An Example',
-    people: [
-        { name: 'Gandalf'},
-        { name: 'Frodo' },
-        { name: 'Hermione'}
-    ]
-};
-
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
-nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+// nunjucks.configure('views'); // point nunjucks to the proper directory for templates
 nunjucks.configure('views', {noCache: true});
-nunjucks.render('index.html', locals, function (err, output) {
-   console.log(output);
-});
+
 app.use(function (req, res, next) {
     // do your logging here
     // call `next`, or else your app will be a black hole — receiving requests but never properly responding
     console.log(req.method)
     next();
-})
-
-app.get("/", function(request, response) {//for webpage
-  //response.send("Hello World!");
-  response.render('index.html')
 })
 
 app.listen(3000, function() {//for terminal
