2e52ea735f1afbc1543008d6841cb772e454c522
Ella Pitassi <ellapitassi23@gmail.com>
2017-09-19 10:39:20 -0400

    finished adding middleware

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, function() {
+app.use(function (req, res, next) {
+    // do your logging here
+    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+    console.log(req.method)
+    next();
+})
+
+app.listen(3000, function() {//for terminal
   console.log("Listening on port 3000...");
 });
 
-app.get("/", function(request, response) {
+app.get("/", function(request, response) {//for webpage
   response.send("Hello World!");
 })
+
