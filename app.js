e40cebbb400eedbc9ada65c2bbee2c6cd96c4d8d
Ella Pitassi <ellapitassi23@gmail.com>
2017-09-19 11:31:19 -0400

    finished implementing nunjucks

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
+const nunjucks = require('nunjucks');
 
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function (err, output) {
+   console.log(output);
+});
 app.use(function (req, res, next) {
     // do your logging here
     // call `next`, or else your app will be a black hole — receiving requests but never properly responding
@@ -8,11 +25,11 @@ app.use(function (req, res, next) {
     next();
 })
 
+app.get("/", function(request, response) {//for webpage
+  //response.send("Hello World!");
+  response.render('index.html')
+})
+
 app.listen(3000, function() {//for terminal
   console.log("Listening on port 3000...");
 });
-
-app.get("/", function(request, response) {//for webpage
-  response.send("Hello World!");
-})
-
